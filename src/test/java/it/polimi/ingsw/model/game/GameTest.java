23c62a8c1ae0233f02844c9ca7811ab6ceb22c24
Marco <marco9.carminati@mail.polimi.it>
2023-04-16 19:49:18 +0200

    Game test: OnPlayerTileSelection #4

--- src/test/java/it/polimi/ingsw/model/game/GameTest.java
+++ src/test/java/it/polimi/ingsw/model/game/GameTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -127,10 +129,10 @@ public class GameTest {
         game.addPlayer("B");
 
         game.onGameStarted();
+
         Coordinate coords1 = new Coordinate(3,1);
         Coordinate coords2 = new Coordinate(4,1);
-        Coordinate coords3 = new Coordinate(5,1);
-        Set<Coordinate> testingCoordinates = Set.of(coords1,coords2,coords3);
+        Set<Coordinate> testingCoordinates = Set.of(coords1,coords2);
 
         assertTrue(game.isSelectionValid(testingCoordinates));
     }
@@ -147,12 +149,104 @@ public class GameTest {
         game.addPlayer("B");
 
         game.onGameStarted();
-        Coordinate coords1 = new Coordinate(1,3);
+
+        Coordinate coords1 = new Coordinate(1,5);
         Coordinate coords2 = new Coordinate(1,4);
         Set<Coordinate> testingCoordinates = Set.of(coords1,coords2);
 
         assertTrue(game.isSelectionValid(testingCoordinates));
     }
+    @Test
+    @DisplayName("verify the function isSelectionValid, negatively")
+    @Disabled
+    //FIXME
+    public void test_is_selection_valid_1_negatively()
+    {
+        Game game = new Game(GameMode.GAME_MODE_2_PLAYERS);
 
+        game.addPlayer("A");
+        game.addPlayer("B");
+
+        game.onGameStarted();
+
+        Coordinate coords1 = new Coordinate(7,1);
+        Coordinate coords2 = new Coordinate(7,2);
+        Set<Coordinate> testingCoordinates = Set.of(coords1,coords2);
+
+        assertFalse(game.isSelectionValid(testingCoordinates));
+    }
+
+    @Test
+    @DisplayName("verify the function isSelectionValid, negatively")
+    @Disabled
+    //FIXME
+    public void test_is_selection_valid_2_negatively()
+    {
+        Game game = new Game(GameMode.GAME_MODE_3_PLAYERS);
+
+        game.addPlayer("A");
+        game.addPlayer("B");
+        game.addPlayer("C");
+
+        game.onGameStarted();
+
+        Coordinate coords1 = new Coordinate(1,4);
+        Coordinate coords2 = new Coordinate(1,5);
+        Coordinate coords3 = new Coordinate(1,6);
+
+        Set<Coordinate> testingCoordinates = Set.of(coords1,coords2,coords3);
+
+        assertFalse(game.isSelectionValid(testingCoordinates));
+    }
+
+    @Test
+    @DisplayName("verify the function onPlayerTileSelection, positively")
+    @Disabled
+    public void test_on_player_tile_selection_1_positively()
+    {
+        Game game = new Game(GameMode.GAME_MODE_2_PLAYERS);
+
+        game.addPlayer("A");
+        game.addPlayer("B");
+
+        game.onGameStarted();
+
+        Coordinate coords1 = new Coordinate(7,3);
+        Coordinate coords2 = new Coordinate(7,4);
+
+        Set<Coordinate> testingCoordinates = Set.of(coords1,coords2);
+
+        game.onPlayerSelectionPhase(testingCoordinates);
+        List<Tile> testingtileslist = List.of(game.getGameMatrix()[coords1.x()][coords1.y()],
+                                              game.getGameMatrix()[coords2.x()][coords2.y()]);
+
+        assertTrue(game.getCurrentPlayer().getPlayerTileSelection().selectionEquals(testingtileslist));
+
+    }
+
+    @Test
+    @DisplayName("verify the function onPlayerTileSelection, negatively")
+    @Disabled
+    public void test_on_player_tile_selection_1_negatively()
+    {
+        Game game = new Game(GameMode.GAME_MODE_2_PLAYERS);
+
+        game.addPlayer("A");
+        game.addPlayer("B");
+
+        game.onGameStarted();
+
+        Coordinate coords1 = new Coordinate(5,1);
+        Coordinate coords2 = new Coordinate(5,2);
+
+        Set<Coordinate> testingCoordinates = Set.of(coords1,coords2);
+
+        game.onPlayerSelectionPhase(testingCoordinates);
+        List<Tile> testingtileslist = List.of(game.getGameMatrix()[coords1.x()][coords1.y()],
+                                              game.getGameMatrix()[coords2.x()][coords2.y()]);
+
+        assertFalse(game.getCurrentPlayer().getPlayerTileSelection().selectionEquals(testingtileslist));
+
+    }
 
 }
