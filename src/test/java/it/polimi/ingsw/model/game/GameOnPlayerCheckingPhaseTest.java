b91d346013249b491e00948a7e75d052965e2b4c
GiuliaBortone <giulia.bortone@mail.polimi.it>
2023-04-19 17:58:26 +0200

    Working on fixing tests

--- src/test/java/it/polimi/ingsw/model/game/GameOnPlayerCheckingPhaseTest.java
+++ src/test/java/it/polimi/ingsw/model/game/GameOnPlayerCheckingPhaseTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GameOnPlayerCheckingPhaseTest implements GameTester {
 
     private Game game;
-
+    /*
     @BeforeEach()
     void init() {
         game = new Game(GameMode.GAME_MODE_4_PLAYERS);
@@ -44,12 +44,41 @@ public class GameOnPlayerCheckingPhaseTest implements GameTester {
         game.onPlayerInsertionPhase(1, tiles);
     }
 
+     */
+
     @Test
     @DisplayName("verify the function OnPlayerCheckingPhase, positively")
     public void test_OnPlayerCheckingPhase_positively() {
+        game = new Game(GameMode.GAME_MODE_4_PLAYERS);
+
+        game.addPlayer(PLAYER_A);
+        game.addPlayer(PLAYER_B);
+        game.addPlayer(PLAYER_C);
+        game.addPlayer(PLAYER_D);
+
+        game.onGameStarted();
+
+        Coordinate c1 = new Coordinate(1, 3);
+        Coordinate c2 = new Coordinate(1, 4);
+
+        Set<Coordinate> selection = Set.of(c1, c2);
+
+        game.onPlayerSelectionPhase(selection);
+
+
+        Tile[][] matrix = game.getGameMatrix();
+
+        List<Tile> tiles = List.of(
+                matrix[c1.x()][c1.y()],
+                matrix[c2.x()][c2.y()]
+        );
+
+        game.onPlayerInsertionPhase(1, tiles);
         game.onPlayerCheckingPhase();
 
         assertFalse(game.getCurrentPlayer().getBookshelf().isFull());
         assertNull(game.getCurrentPlayer().getAcquiredTokens());
+
     }
+
 }
