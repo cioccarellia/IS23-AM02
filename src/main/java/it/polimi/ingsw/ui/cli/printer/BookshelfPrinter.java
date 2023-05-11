b03c368b9b411793dbb76ed67b7be0268fb16742
Marco <marco9.carminati@mail.polimi.it>
2023-05-12 01:32:29 +0200

    CLI implementation

--- src/main/java/it/polimi/ingsw/ui/cli/printer/BookshelfPrinter.java
+++ src/main/java/it/polimi/ingsw/ui/cli/printer/BookshelfPrinter.java
@@ -1,26 +1,72 @@
 package it.polimi.ingsw.ui.cli.printer;
 
+import it.polimi.ingsw.model.board.Tile;
 import it.polimi.ingsw.model.bookshelf.Bookshelf;
 import it.polimi.ingsw.model.config.bookshelf.BookshelfConfiguration;
 import it.polimi.ingsw.ui.cli.Console;
+
+
+import java.util.Arrays;
+
+import static it.polimi.ingsw.model.board.Tile.CAT;
 
 public class BookshelfPrinter {
 
     private static final int rows = BookshelfConfiguration.getInstance().rows();
     private static final int cols = BookshelfConfiguration.getInstance().cols();
 
+    public static final Tile[][] BOOKSHELF_MATRIX = {
+            {CAT, CAT, CAT, CAT, CAT},
+            {CAT, CAT, CAT, CAT, CAT},
+            {CAT, CAT, CAT, CAT, CAT},
+            {CAT, CAT, CAT, CAT, CAT},
+            {CAT, CAT, CAT, CAT, CAT},
+            {CAT, CAT, CAT, CAT, CAT}
+
+    };
+
     public static String print(Bookshelf bookshelf) {
         StringBuilder builder = new StringBuilder();
         int verticalGuideNumber = 0;
 
+        Tile[][] matrix = new Tile[rows][cols];
+        Arrays.deepEquals(matrix, bookshelf.getShelfMatrix());
+
         Console.out(" 0  1  2  3  4\n");
         for (int i = 0; i < rows; i++) {
+
+            Console.out(verticalGuideNumber);
+            Console.out(" ");
+
             for (int j = 0; j < cols; j++) {
 
+                if(matrix[i][j] != null)
+                {
+                    String tileText = TilePrinter.print(matrix[i][j]);
+
+                    Console.out(" " + tileText + " ");
+                }
+                else
+                {
+                    Console.out("   ");
+                }
             }
+
+            Console.out("\n");
+            verticalGuideNumber++;
         }
 
-        return null; //
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        var bookshelf = new Bookshelf();
+
+        bookshelf.fillUpBookShelf(BOOKSHELF_MATRIX);
+
+        print(bookshelf);
+
+        System.out.flush();
     }
 
 }
