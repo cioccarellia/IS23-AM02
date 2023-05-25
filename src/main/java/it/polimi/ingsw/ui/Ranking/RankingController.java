8730fc0d2c1fed413fe24249f7b2386bb81d06aa
Marco <marco9.carminati@mail.polimi.it>
2023-05-25 12:24:03 +0200

    Gui implementation: LobbyController RankingController

---
+++ src/main/java/it/polimi/ingsw/ui/Ranking/RankingController.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.ui.Ranking;
+
+import it.polimi.ingsw.ui.game.GameGateway;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RankingController extends Application implements GameGateway {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    
+
+}
