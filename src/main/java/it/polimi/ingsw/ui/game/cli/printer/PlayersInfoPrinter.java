10505978f241307451d5a232c30861a994f6e809
GiuliaBortone <giulia.bortone@mail.polimi.it>
2023-06-29 20:11:08 +0200

    Updated list of players in cli

--- src/main/java/it/polimi/ingsw/ui/game/cli/printer/PlayersInfoPrinter.java
+++ src/main/java/it/polimi/ingsw/ui/game/cli/printer/PlayersInfoPrinter.java
@@ -9,28 +9,34 @@ import java.util.List;
 
 public class PlayersInfoPrinter {
     public static void print(List<PlayerSession> playersSessions, List<PlayerInfo> playerInfo, List<PlayerScore> scores) {
-
         System.out.print("List of players:");
         System.out.println();
-        System.out.print("Player                |Role   |Connection   |Game phase   |Total points   |");
+        System.out.print("Player                |Game phase   |Total points   |");
+        if (playerInfo != null) {
+            System.out.print("Role   |Connection   |");
+        }
         System.out.println();
-
 
         for (PlayerSession session : playersSessions) {
             StringBuilder text = new StringBuilder();
             String username = session.getUsername();
-            PlayerInfo info = (playerInfo != null ? playerInfo.stream().filter(player -> player.username().equals(username)).findAny().get() : null);
 
-            String role = (info != null ? (info.isHost() ? "Host" : "Player") : "");
-            String connection = (info != null ? info.status().toString().toLowerCase() : "");
             String gamePhase = session.getPlayerCurrentGamePhase().toString().toLowerCase();
             String points = String.valueOf(scores.stream().filter(s -> s.username().equals(username)).findAny().get().total());
 
             text.append(StringUtils.rightPad(username, 22, "")).append("|");
-            text.append(StringUtils.rightPad(role, 7, "")).append("|");
-            text.append(StringUtils.rightPad(connection, 13, "")).append("|");
             text.append(StringUtils.rightPad(gamePhase, 13, "")).append("|");
             text.append(StringUtils.rightPad(points, 15, "")).append("|");
+
+            if (playerInfo != null) {
+                PlayerInfo info = playerInfo.stream().filter(player -> player.username().equals(username)).findAny().get();
+
+                String role = (info.isHost() ? "Host" : "Player");
+                String connection = info.status().toString().toLowerCase();
+
+                text.append(StringUtils.rightPad(role, 7, "")).append("|");
+                text.append(StringUtils.rightPad(connection, 13, "")).append("|");
+            }
 
             System.out.print(text);
             System.out.println();
